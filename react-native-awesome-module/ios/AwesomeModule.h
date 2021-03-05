27a25c7a2d10caf9b61463375c0c507844981f74
Saily <saily.webvillee@gmail.com>
2021-03-05 18:05:47 +0530

    remove folder

--- react-native-awesome-module/ios/AwesomeModule.h
+++
@@ -1,5 +0,0 @@
-#import <React/RCTBridgeModule.h>
-
-@interface AwesomeModule : NSObject <RCTBridgeModule>
-
-@end
--- react-native-awesome-module/src/index.tsx
+++
@@ -1,9 +0,0 @@
-import { NativeModules } from 'react-native';
-
-type AwesomeModuleType = {
-  multiply(a: number, b: number): Promise<number>;
-};
-
-const { AwesomeModule } = NativeModules;
-
-export default AwesomeModule as AwesomeModuleType;
